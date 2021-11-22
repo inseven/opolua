ccf942d05e7dd5b814ee827a9f7a6bfb1cfc663d
Jason Barrie Morley <jbmorley@mac.com>
2021-11-22 13:17:00 -0800

    chore: Add the license to the Swift files

--- OpoLua/AppDelegate.swift
+++ OpoLua/AppDelegate.swift
@@ -1,52 +1,24 @@
+// Copyright (c) 2021 Jason Morley, Tom Sutcliffe
 //
-//  AppDelegate.swift
-//  OpoLua
+// Permission is hereby granted, free of charge, to any person obtaining a copy
+// of this software and associated documentation files (the "Software"), to deal
+// in the Software without restriction, including without limitation the rights
+// to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+// copies of the Software, and to permit persons to whom the Software is
+// furnished to do so, subject to the following conditions:
 //
-//  Created by Tom Sutcliffe on 15/11/2021.
+// The above copyright notice and this permission notice shall be included in all
+// copies or substantial portions of the Software.
 //
+// THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+// IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+// FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+// AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+// LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+// OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+// SOFTWARE.
 
 import UIKit
-
-//import Cocoa
-//
-//func f(_ L: OpaquePointer?) -> Int32 {
-//    print("Wowsers this worked!")
-//    let arg = String(validatingUTF8: luaL_tolstring(L, 1, nil)!)
-//    print("arg1=\(arg!)")
-//    return 0
-//}
-//
-//func lua_pushfstring(_ L: OpaquePointer?, fmt: UnsafePointer<CChar>, arguments:CVarArg...) -> UnsafePointer<CChar> {
-//    return withVaList(arguments) { va_list in
-//        lua_pushvfstring(L, fmt, va_list)
-//    }
-//}
-//
-//@main
-//class AppDelegate: NSObject, NSApplicationDelegate {
-//
-//    @IBOutlet var window: NSWindow!
-//    var L: OpaquePointer?
-//
-//    func applicationWillFinishLaunching(_ aNotification: Notification) {
-//        L = luaL_newstate()
-////        lua_pushcfunction(L) { L in
-////            print("Wowsers this worked")
-////            return 0
-////        }
-//        lua_pushcfunction(L, f)
-//        let _ = lua_pushfstring(L, fmt: "%d", arguments: 1)
-//    }
-//
-//    func applicationDidFinishLaunching(_ notification: Notification) {
-//        lua_callk(L, 1, 0, 0, nil)
-//    }
-//    func applicationWillTerminate(_ aNotification: Notification) {
-//        // Insert code here to tear down your application
-//    }
-//
-//
-//}
 
 @main
 class AppDelegate: UIResponder, UIApplicationDelegate {
