2a084e9034efcd3b624fc34fcb824c8679df75f5
Tom Sutcliffe <tomsci@me.com>
2021-11-24 22:39:48 +0000

    Add minimal toolbar.lua module (implementing TOOLBAR.OPO interface)

---
+++ src/modules/toolbar.lua
@@ -0,0 +1,35 @@
+_ENV = module()
+
+function TBarLink(runtime, appLink)
+    runtime:callProc(appLink:upper())
+end
+
+function TBarInit(runtime, title, scrW, scrH)
+    --TODO
+end
+
+function TBarSetTitle(runtime, name)
+    --TODO
+end
+
+function TBarButt(runtime, shortcut, pos, text, state, bit, mask, flags)
+    --TODO
+end
+
+function TBarOffer(runtime, winId, ptrType, ptrX, ptrY)
+    --TODO
+end
+
+function TBarLatch(runtime, comp)
+    --TODO
+end
+
+function TBarShow(runtime)
+    --TODO
+end
+
+function TBarHide(runtime)
+    --TODO
+end
+
+return _ENV
