b83268553e1e1d60f05df2f3db38131721654ed3
Jason Barrie Morley <jbmorley@mac.com>
2022-01-19 17:56:30 -0800

    Remove unused `HeaderView`

--- OpoLua/View Controllers/LibraryViewController.swift
+++ OpoLua/View Controllers/LibraryViewController.swift
@@ -61,32 +61,6 @@ class LibraryViewController: UIViewController {
 
     class LibraryDataSource: UICollectionViewDiffableDataSource<Section, Item> {}
 
-    class HeaderView: UICollectionReusableView {
-
-        lazy var textLabel: UILabel = {
-            let label = UILabel()
-            label.text = "Cheese"
-            label.translatesAutoresizingMaskIntoConstraints = false
-            return label
-        }()
-
-        override init(frame: CGRect) {
-            super.init(frame: frame)
-            addSubview(textLabel)
-            NSLayoutConstraint.activate([
-                textLabel.leadingAnchor.constraint(equalTo: layoutMarginsGuide.leadingAnchor),
-                textLabel.trailingAnchor.constraint(equalTo: layoutMarginsGuide.trailingAnchor),
-                textLabel.topAnchor.constraint(equalTo: layoutMarginsGuide.topAnchor),
-                textLabel.bottomAnchor.constraint(equalTo: layoutMarginsGuide.bottomAnchor),
-            ])
-        }
-
-        required init?(coder: NSCoder) {
-            fatalError("init(coder:) has not been implemented")
-        }
-
-    }
-
     var settings: Settings
     var taskManager: TaskManager
     var dataSource: LibraryDataSource!
