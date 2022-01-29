5c44acb3c36275b790ed88c134da61a91c2e4c9e
Tom Sutcliffe <tomsci@me.com>
2022-01-29 13:23:55 +0000

    chore: Add alarm.opx stubs

---
+++ src/opx/alarm.lua
@@ -0,0 +1,160 @@
+--[[
+
+Copyright (c) 2022 Jason Morley, Tom Sutcliffe
+
+Permission is hereby granted, free of charge, to any person obtaining a copy
+of this software and associated documentation files (the "Software"), to deal
+in the Software without restriction, including without limitation the rights
+to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+copies of the Software, and to permit persons to whom the Software is
+furnished to do so, subject to the following conditions:
+
+The above copyright notice and this permission notice shall be included in all
+copies or substantial portions of the Software.
+
+THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+SOFTWARE.
+
+]]
+
+_ENV = module()
+
+fns = {
+    [1] = "AlmSetClockAlarm",
+    [2] = "AlmAlarmState",
+    [3] = "AlmAlarmEnable",
+    [4] = "AlmAlarmDelete",
+    [5] = "AlmQuietPeriodCancel",
+    [6] = "AlmQuietPeriodUntil",
+    [7] = "AlmQuietPeriodSet",
+    [8] = "AlmSetAlarmSound",
+    [9] = "AlmAlarmSoundState",
+    [10] = "WldFindCity",
+    [11] = "WldSunlight",
+    [12] = "WldHome",
+    [13] = "WldPreviousCity",
+    [14] = "WldPreviousCountry",
+    [15] = "WldNextCity",
+    [16] = "WldNextCountry",
+    [17] = "WldAddCity",
+    [18] = "WldAddCountry",
+    [19] = "WldEditCity",
+    [20] = "WldEditCountry",
+    [21] = "WldDeleteCity",
+    [22] = "WldDeleteCountry",
+    [23] = "WldNumberOfCities",
+    [24] = "WldNumberOfCountries",
+    [25] = "WldDataFileSave",
+    [26] = "WldDataFileRevert",
+}
+
+function AlmSetClockAlarm(stack, runtime) -- 1
+    error("Unimplemented alarm.opx function AlmSetClockAlarm!")
+end
+
+function AlmAlarmState(stack, runtime) -- 2
+    error("Unimplemented alarm.opx function AlmAlarmState!")
+end
+
+function AlmAlarmEnable(stack, runtime) -- 3
+    error("Unimplemented alarm.opx function AlmAlarmEnable!")
+end
+
+function AlmAlarmDelete(stack, runtime) -- 4
+    error("Unimplemented alarm.opx function AlmAlarmDelete!")
+end
+
+function AlmQuietPeriodCancel(stack, runtime) -- 5
+    error("Unimplemented alarm.opx function AlmQuietPeriodCancel!")
+end
+
+function AlmQuietPeriodUntil(stack, runtime) -- 6
+    error("Unimplemented alarm.opx function AlmQuietPeriodUntil!")
+end
+
+function AlmQuietPeriodSet(stack, runtime) -- 7
+    error("Unimplemented alarm.opx function AlmQuietPeriodSet!")
+end
+
+function AlmSetAlarmSound(stack, runtime) -- 8
+    error("Unimplemented alarm.opx function AlmSetAlarmSound!")
+end
+
+function AlmAlarmSoundState(stack, runtime) -- 9
+    error("Unimplemented alarm.opx function AlmAlarmSoundState!")
+end
+
+function WldFindCity(stack, runtime) -- 10
+    error("Unimplemented alarm.opx function WldFindCity!")
+end
+
+function WldSunlight(stack, runtime) -- 11
+    error("Unimplemented alarm.opx function WldSunlight!")
+end
+
+function WldHome(stack, runtime) -- 12
+    error("Unimplemented alarm.opx function WldHome!")
+end
+
+function WldPreviousCity(stack, runtime) -- 13
+    error("Unimplemented alarm.opx function WldPreviousCity!")
+end
+
+function WldPreviousCountry(stack, runtime) -- 14
+    error("Unimplemented alarm.opx function WldPreviousCountry!")
+end
+
+function WldNextCity(stack, runtime) -- 15
+    error("Unimplemented alarm.opx function WldNextCity!")
+end
+
+function WldNextCountry(stack, runtime) -- 16
+    error("Unimplemented alarm.opx function WldNextCountry!")
+end
+
+function WldAddCity(stack, runtime) -- 17
+    error("Unimplemented alarm.opx function WldAddCity!")
+end
+
+function WldAddCountry(stack, runtime) -- 18
+    error("Unimplemented alarm.opx function WldAddCountry!")
+end
+
+function WldEditCity(stack, runtime) -- 19
+    error("Unimplemented alarm.opx function WldEditCity!")
+end
+
+function WldEditCountry(stack, runtime) -- 20
+    error("Unimplemented alarm.opx function WldEditCountry!")
+end
+
+function WldDeleteCity(stack, runtime) -- 21
+    error("Unimplemented alarm.opx function WldDeleteCity!")
+end
+
+function WldDeleteCountry(stack, runtime) -- 22
+    error("Unimplemented alarm.opx function WldDeleteCountry!")
+end
+
+function WldNumberOfCities(stack, runtime) -- 23
+    error("Unimplemented alarm.opx function WldNumberOfCities!")
+end
+
+function WldNumberOfCountries(stack, runtime) -- 24
+    error("Unimplemented alarm.opx function WldNumberOfCountries!")
+end
+
+function WldDataFileSave(stack, runtime) -- 25
+    error("Unimplemented alarm.opx function WldDataFileSave!")
+end
+
+function WldDataFileRevert(stack, runtime) -- 26
+    error("Unimplemented alarm.opx function WldDataFileRevert!")
+end
+
+return _ENV
