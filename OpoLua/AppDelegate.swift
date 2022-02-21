74dbfe8c4a17f61256c6d240d5282147c1e18624
Jason Barrie Morley <jbmorley@mac.com>
2022-02-21 08:16:18 -0800

    refactor: Move `UISplitViewController` management into a new subclass

--- OpoLua/AppDelegate.swift
+++ OpoLua/AppDelegate.swift
@@ -29,27 +29,22 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
     }
 
     var window: UIWindow?
-    var section: ApplicationSection?
-    var previousSection: ApplicationSection = .allPrograms  // Represents the previously active section; always the one we expand to when entering split view.
-    // N.B. We only ever store previous section for state restoration as we always want to restore to at least the top-level of the section.
 
-    private var settings = Settings()
+    private let settings: Settings
+    private let taskManager: TaskManager
+    private let detector: ProgramDetector
+    let downloader: UbiquitousDownloader
 
-    private lazy var taskManager: TaskManager = {
-        return TaskManager(settings: settings)
-    }()
+    private var rootViewController: RootViewController!
+    private var settingsSink: AnyCancellable?
 
-    private lazy var detector: ProgramDetector = {
-        return ProgramDetector(settings: settings)
-    }()
-
-    lazy var downloader: UbiquitousDownloader = {
-        return UbiquitousDownloader(settings: settings)
-    }()
-
-    var splitViewController: UISplitViewController!
-    var libraryViewController: LibraryViewController!
-    var settingsSink: AnyCancellable?
+    override init() {
+        settings = Settings()
+        taskManager = TaskManager(settings: settings)
+        detector = ProgramDetector(settings: settings)
+        downloader = UbiquitousDownloader(settings: settings)
+        super.init()
+    }
 
     func application(_ application: UIApplication,
                      didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
@@ -60,20 +55,10 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
             try! fileManager.createDirectory(at: fileManager.documentsUrl, withIntermediateDirectories: true)
         }
 
-        libraryViewController = LibraryViewController(settings: settings, taskManager: taskManager, detector: detector)
-        libraryViewController.delegate = self
-        let navigationController = UINavigationController(rootViewController: libraryViewController)
-        navigationController.navigationBar.prefersLargeTitles = true
-        navigationController.delegate = self
-
-        splitViewController = UISplitViewController(style: .doubleColumn)
-        splitViewController.delegate = self
-        splitViewController.preferredDisplayMode = .automatic
-        splitViewController.setViewController(navigationController, for: .primary)
-        showSection(.allPrograms)
+        rootViewController = RootViewController(settings: settings, taskManager: taskManager, detector: detector)
 
         window = UIWindow()
-        window?.rootViewController = splitViewController
+        window?.rootViewController = rootViewController
         window?.tintColor = settings.theme.color
         window?.makeKeyAndVisible()
 
@@ -93,7 +78,7 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
                      open url: URL,
                      options: [UIApplication.OpenURLOptionsKey : Any] = [:]) -> Bool {
         guard url.startAccessingSecurityScopedResource() else {
-            splitViewController.present(error: OpoLuaError.secureAccess)
+            rootViewController.present(error: OpoLuaError.secureAccess)
             return false
         }
         install(url: url)
@@ -105,110 +90,7 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
                                                               url: url,
                                                               preferredDestinationUrl: preferredDestinationUrl)
         installerViewController.installerDelegate = self
-        splitViewController.present(installerViewController, animated: true)
-    }
-
-    func setRootDetailViewController(_ viewController: UIViewController, animated: Bool = true) {
-        if splitViewController.isCollapsed {
-            let navigationController = splitViewController.viewControllers[0] as! UINavigationController
-            navigationController.setViewControllers([navigationController.viewControllers[0], viewController],
-                                                    animated: animated)
-        } else {
-            let navigationController = UINavigationController(rootViewController: viewController)
-            splitViewController.setViewController(navigationController, for: .secondary)
-        }
-    }
-
-    func showDirectory(url: URL, animated: Bool) {
-        let directory = Directory(url: url)
-        let viewController = DirectoryViewController(settings: settings, taskManager: taskManager, directory: directory)
-        setRootDetailViewController(viewController, animated: animated)
-    }
-
-    func showSection(_ section: ApplicationSection, animated: Bool = true) {
-        guard self.section != section else {
-            if splitViewController.isCollapsed {
-                if let navigationController = splitViewController.viewControllers[0] as? UINavigationController {
-                    let viewController = navigationController.viewControllers[1]
-                    navigationController.popToViewController(viewController, animated: animated)
-                }
-            } else {
-                if splitViewController.viewControllers.count > 1,
-                   let detailNavigationController = splitViewController.viewControllers[1] as? UINavigationController {
-                    detailNavigationController.popToRootViewController(animated: animated)
-                }
-            }
-            return
-        }
-        self.section = section
-
-        switch section {
-        case .runningPrograms:
-            let viewController = RunningProgramsViewController(settings: settings, taskManager: taskManager)
-            setRootDetailViewController(viewController)
-        case .allPrograms:
-            let viewController = AllProgramsViewController(settings: settings, taskManager: taskManager, detector: detector)
-            setRootDetailViewController(viewController, animated: animated)
-        case .documents:
-            showDirectory(url: FileManager.default.documentsUrl, animated: animated)
-        case .local(let url):
-            showDirectory(url: url, animated: animated)
-        case .external(let url):
-            showDirectory(url: url, animated: animated)
-        }
-    }
-
-    private var activeViewController: UIViewController {
-        return detailNavigationController.viewControllers.last!
-    }
-
-    private var detailNavigationController: UINavigationController {
-        if splitViewController.isCollapsed {
-            return splitViewController.viewControllers[0] as! UINavigationController
-        } else {
-            return splitViewController.viewControllers[1] as! UINavigationController
-        }
-    }
-
-    func section(for url: URL) -> (ApplicationSection, [URL])? {
-        guard let location = settings.indexableUrls.first(where: {
-            url.path.starts(with: $0.path) || url.resolvingSymlinksInPath().path.starts(with: $0.path)
-        }) else {
-            return nil
-        }
-        let components = String(url.path.dropFirst(location.path.count)).split(separator: "/").map { String($0) }
-        var directoryUrls: [URL] = []
-        var loopUrl = location
-        for component in components {
-            loopUrl = loopUrl.appendingPathComponent(component)
-            directoryUrls.append(loopUrl)
-        }
-        switch location {
-        case Bundle.main.filesUrl, Bundle.main.scriptsUrl, Bundle.main.testsUrl:
-            return (ApplicationSection.local(location), directoryUrls)
-        case FileManager.default.documentsUrl:
-            return (ApplicationSection.documents, [])
-        default:
-            return (ApplicationSection.external(location), directoryUrls)
-        }
-    }
-
-    func showUrl(_ url: URL) {
-        if let activeDirectoryViewController = activeViewController as? DirectoryViewController,
-           activeDirectoryViewController.directory.url == url {
-            print("Nothing to do!")
-            return
-        }
-        guard let (section, urls) = section(for: url) else {
-            print("Unable to determine section for '\(url)'.")
-            return
-        }
-        self.showSection(section, animated: false)
-        libraryViewController.selectSection(section: section)
-        urls.map { DirectoryViewController(settings: settings,
-                                           taskManager: taskManager,
-                                           directory: Directory(url: $0)) }
-        .forEach { detailNavigationController.pushViewController($0, animated: false) }
+        rootViewController.present(installerViewController, animated: true)
     }
 
     func runApplication(_ applicationIdentifier: ApplicationIdentifier, url: URL) -> Int32 {
@@ -218,9 +100,13 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
             let viewController = SourceViewController(url: url, showsDoneButton: true)
             viewController.delegate = self
             let navigationController = UINavigationController(rootViewController: viewController)
-            splitViewController.present(navigationController, animated: true)
+            rootViewController.present(navigationController, animated: true)
             return 1
         }
+    }
+
+    func showUrl(_ url: URL) {
+        rootViewController.showUrl(url)
     }
     
 }
@@ -244,63 +130,9 @@ extension AppDelegate: InstallerViewControllerDelegate {
                                  didInstallToDestinationUrl destinationUrl: URL) {
         dispatchPrecondition(condition: .onQueue(.main))
         installerViewController.dismiss(animated: true)
-        showUrl(destinationUrl)
+        rootViewController.showUrl(destinationUrl)
     }
 
-}
-
-extension AppDelegate: UISplitViewControllerDelegate {
-
-    func splitViewController(_ svc: UISplitViewController,
-                             topColumnForCollapsingToProposedTopColumn proposedTopColumn: UISplitViewController.Column) -> UISplitViewController.Column {
-        print("splitViewcontroller(\(svc), topColumnForCollapsingToProposedColumn: \(proposedTopColumn.description))")
-        return proposedTopColumn
-    }
-
-    func splitViewController(_ svc: UISplitViewController, willHide column: UISplitViewController.Column) {
-        print("splitViewController(\(svc), willHide: \(column.description))")
-    }
-
-    func splitViewControllerDidCollapse(_ svc: UISplitViewController) {
-        print("splitViewControllerDidCollapse(\(svc)")
-        print(splitViewController.viewControllers)
-    }
-
-    func splitViewController(_ svc: UISplitViewController,
-                             displayModeForExpandingToProposedDisplayMode proposedDisplayMode: UISplitViewController.DisplayMode) -> UISplitViewController.DisplayMode {
-        print("splitViewController(\(svc), displayModeForExpandingToProposedDisplayMode: \(proposedDisplayMode.description))")
-        return proposedDisplayMode
-    }
-
-    func splitViewController(_ svc: UISplitViewController, willShow column: UISplitViewController.Column) {
-        print("splitViewController(\(svc), willShow: \(column.description))")
-    }
-
-    func splitViewControllerDidExpand(_ svc: UISplitViewController) {
-        print("splitViewControllerDidExpand(\(svc))")
-        guard let primaryNavigationController = splitViewController.viewControllers[0] as? UINavigationController else {
-            return
-        }
-        if let viewControllers = primaryNavigationController.popToRootViewController(animated: false) {
-            let navigationController = UINavigationController(rootViewController: viewControllers.first!)
-            for viewController in viewControllers[1...] {
-                navigationController.pushViewController(viewController, animated: false)
-            }
-            self.splitViewController.setViewController(navigationController, for: .secondary)
-        }
-        if let section = section {
-            libraryViewController.selectSection(section: section)
-        }
-    }
-
-}
-
-extension AppDelegate: LibraryViewControllerDelegate {
-
-    func libraryViewController(_ libraryViewController: LibraryViewController,
-                               showSection section: ApplicationSection) {
-        showSection(section)
-    }
 }
 
 extension AppDelegate: TaskManagerDelegate {
@@ -308,48 +140,12 @@ extension AppDelegate: TaskManagerDelegate {
     func taskManagerShowTaskList(_ taskManager: TaskManager) {
         let taskManagerViewController = TaskManagerViewController(settings: settings, taskManager: taskManager)
         let navigationController = UINavigationController(rootViewController: taskManagerViewController)
-        splitViewController.present(navigationController, animated: true)
+        rootViewController.present(navigationController, animated: true)
     }
 
     func taskManager(_ taskManager: TaskManager, bringProgramToForeground program: Program) {
-        let activeViewController = self.activeViewController
-        if let programViewController = activeViewController as? ProgramViewController,
-           programViewController.program.url == program.url {
-            return
-        }
-        let programViewController = ProgramViewController(settings: settings,
-                                                          taskManager: taskManager,
-                                                          program: program)
-        section = .runningPrograms
-        libraryViewController.selectSection(section: .runningPrograms)
-        let runningProgramsViewController = RunningProgramsViewController(settings: settings, taskManager: taskManager)
-        if splitViewController.isCollapsed {
-            let navigationController = splitViewController.viewControllers[0] as! UINavigationController
-            navigationController.setViewControllers([libraryViewController,
-                                                     runningProgramsViewController,
-                                                     programViewController],
-                                                    animated: false)
-        } else {
-            let navigationController = splitViewController.viewControllers[1] as! UINavigationController
-            navigationController.setViewControllers([runningProgramsViewController, programViewController],
-                                                    animated: false)
-        }
+        rootViewController.bringProgramToForeground(program)
     }
 
 }
 
-extension AppDelegate: UINavigationControllerDelegate {
-
-    func navigationController(_ navigationController: UINavigationController,
-                              willShow viewController: UIViewController,
-                              animated: Bool) {
-        // Detect navigation to the root view when collapsed as this indicates there's no section selected.
-        // We also store the previously selected section to ensure we can return to this when expanding.
-        guard splitViewController.isCollapsed, viewController == libraryViewController, let section = section else {
-            return
-        }
-        previousSection = section
-        self.section = nil
-    }
-
-}
---
+++ OpoLua/View Controllers/RootViewController.swift
@@ -0,0 +1,258 @@
+// Copyright (c) 2021-2022 Jason Morley, Tom Sutcliffe
+//
+// Permission is hereby granted, free of charge, to any person obtaining a copy
+// of this software and associated documentation files (the "Software"), to deal
+// in the Software without restriction, including without limitation the rights
+// to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+// copies of the Software, and to permit persons to whom the Software is
+// furnished to do so, subject to the following conditions:
+//
+// The above copyright notice and this permission notice shall be included in all
+// copies or substantial portions of the Software.
+//
+// THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+// IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+// FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+// AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+// LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+// OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+// SOFTWARE.
+
+import Combine
+import UIKit
+
+class RootViewController: UISplitViewController {
+
+    private let settings: Settings
+    private var taskManager: TaskManager
+    private var detector: ProgramDetector
+    private var libraryViewController: LibraryViewController
+
+    private var section: ApplicationSection?
+    private var previousSection: ApplicationSection = .allPrograms
+    // Represents the previously active section; always the one we expand to when entering split view.
+    // N.B. We only ever store previous section for state restoration as we always want to restore to at least the
+    // top-level of the section.
+
+    private var activeViewController: UIViewController {
+        return detailNavigationController.viewControllers.last!
+    }
+
+    private var detailNavigationController: UINavigationController {
+        if isCollapsed {
+            return viewControllers[0] as! UINavigationController
+        } else {
+            return viewControllers[1] as! UINavigationController
+        }
+    }
+
+    init(settings: Settings, taskManager: TaskManager, detector: ProgramDetector) {
+        self.settings = settings
+        self.taskManager = taskManager
+        self.detector = detector
+        self.libraryViewController = LibraryViewController(settings: settings,
+                                                           taskManager: taskManager,
+                                                           detector: detector)
+        super.init(style: .doubleColumn)
+        libraryViewController.delegate = self
+        delegate = self
+        preferredDisplayMode = .automatic
+    }
+
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        let navigationController = UINavigationController(rootViewController: libraryViewController)
+        navigationController.navigationBar.prefersLargeTitles = true
+        navigationController.delegate = self
+        setViewController(navigationController, for: .primary)
+        showSection(.allPrograms)
+    }
+
+    func setRootDetailViewController(_ viewController: UIViewController, animated: Bool = true) {
+        if isCollapsed {
+            let navigationController = viewControllers[0] as! UINavigationController
+            navigationController.setViewControllers([navigationController.viewControllers[0], viewController],
+                                                    animated: animated)
+        } else {
+            let navigationController = UINavigationController(rootViewController: viewController)
+            setViewController(navigationController, for: .secondary)
+        }
+    }
+
+    func showSection(_ section: ApplicationSection, animated: Bool = true) {
+        guard self.section != section else {
+            if isCollapsed {
+                if let navigationController = viewControllers[0] as? UINavigationController {
+                    let viewController = navigationController.viewControllers[1]
+                    navigationController.popToViewController(viewController, animated: animated)
+                }
+            } else {
+                if viewControllers.count > 1,
+                   let detailNavigationController = viewControllers[1] as? UINavigationController {
+                    detailNavigationController.popToRootViewController(animated: animated)
+                }
+            }
+            return
+        }
+        self.section = section
+
+        switch section {
+        case .runningPrograms:
+            let viewController = RunningProgramsViewController(settings: settings, taskManager: taskManager)
+            setRootDetailViewController(viewController)
+        case .allPrograms:
+            let viewController = AllProgramsViewController(settings: settings,
+                                                           taskManager: taskManager,
+                                                           detector: detector)
+            setRootDetailViewController(viewController, animated: animated)
+        case .documents:
+            showDirectory(url: FileManager.default.documentsUrl, animated: animated)
+        case .local(let url):
+            showDirectory(url: url, animated: animated)
+        case .external(let url):
+            showDirectory(url: url, animated: animated)
+        }
+    }
+
+    func showUrl(_ url: URL) {
+        if let activeDirectoryViewController = activeViewController as? DirectoryViewController,
+           activeDirectoryViewController.directory.url == url {
+            print("Nothing to do!")
+            return
+        }
+        guard let (section, urls) = section(for: url) else {
+            print("Unable to determine section for '\(url)'.")
+            return
+        }
+        self.showSection(section, animated: false)
+        libraryViewController.selectSection(section: section)
+        urls.map { DirectoryViewController(settings: settings,
+                                           taskManager: taskManager,
+                                           directory: Directory(url: $0)) }
+        .forEach { detailNavigationController.pushViewController($0, animated: false) }
+    }
+
+    func section(for url: URL) -> (ApplicationSection, [URL])? {
+        guard let location = settings.indexableUrls.first(where: {
+            url.path.starts(with: $0.path) || url.resolvingSymlinksInPath().path.starts(with: $0.path)
+        }) else {
+            return nil
+        }
+        let components = String(url.path.dropFirst(location.path.count)).split(separator: "/").map { String($0) }
+        var directoryUrls: [URL] = []
+        var loopUrl = location
+        for component in components {
+            loopUrl = loopUrl.appendingPathComponent(component)
+            directoryUrls.append(loopUrl)
+        }
+        switch location {
+        case Bundle.main.filesUrl, Bundle.main.scriptsUrl, Bundle.main.testsUrl:
+            return (ApplicationSection.local(location), directoryUrls)
+        case FileManager.default.documentsUrl:
+            return (ApplicationSection.documents, [])
+        default:
+            return (ApplicationSection.external(location), directoryUrls)
+        }
+    }
+
+    func bringProgramToForeground(_ program: Program) {
+        let activeViewController = self.activeViewController
+        if let programViewController = activeViewController as? ProgramViewController,
+           programViewController.program.url == program.url {
+            return
+        }
+        let programViewController = ProgramViewController(settings: settings,
+                                                          taskManager: taskManager,
+                                                          program: program)
+        section = .runningPrograms
+        libraryViewController.selectSection(section: .runningPrograms)
+        let runningProgramsViewController = RunningProgramsViewController(settings: settings, taskManager: taskManager)
+        if isCollapsed {
+            let navigationController = viewControllers[0] as! UINavigationController
+            navigationController.setViewControllers([libraryViewController,
+                                                     runningProgramsViewController,
+                                                     programViewController],
+                                                    animated: false)
+        } else {
+            let navigationController = viewControllers[1] as! UINavigationController
+            navigationController.setViewControllers([runningProgramsViewController, programViewController],
+                                                    animated: false)
+        }
+    }
+
+}
+
+extension RootViewController: UISplitViewControllerDelegate {
+
+    func splitViewController(_ svc: UISplitViewController,
+                             topColumnForCollapsingToProposedTopColumn proposedTopColumn: UISplitViewController.Column) -> UISplitViewController.Column {
+        return proposedTopColumn
+    }
+
+    func splitViewController(_ svc: UISplitViewController, willHide column: UISplitViewController.Column) {
+    }
+
+    func splitViewControllerDidCollapse(_ svc: UISplitViewController) {
+    }
+
+    func splitViewController(_ svc: UISplitViewController,
+                             displayModeForExpandingToProposedDisplayMode proposedDisplayMode: UISplitViewController.DisplayMode) -> UISplitViewController.DisplayMode {
+        return proposedDisplayMode
+    }
+
+    func splitViewController(_ svc: UISplitViewController, willShow column: UISplitViewController.Column) {
+    }
+
+    func splitViewControllerDidExpand(_ svc: UISplitViewController) {
+        guard let primaryNavigationController = viewControllers[0] as? UINavigationController else {
+            return
+        }
+        if let viewControllers = primaryNavigationController.popToRootViewController(animated: false) {
+            let navigationController = UINavigationController(rootViewController: viewControllers.first!)
+            for viewController in viewControllers[1...] {
+                navigationController.pushViewController(viewController, animated: false)
+            }
+            setViewController(navigationController, for: .secondary)
+        }
+        if let section = section {
+            libraryViewController.selectSection(section: section)
+        }
+    }
+
+    func showDirectory(url: URL, animated: Bool) {
+        let directory = Directory(url: url)
+        let viewController = DirectoryViewController(settings: settings, taskManager: taskManager, directory: directory)
+        setRootDetailViewController(viewController, animated: animated)
+    }
+
+}
+
+extension RootViewController: LibraryViewControllerDelegate {
+
+    func libraryViewController(_ libraryViewController: LibraryViewController,
+                               showSection section: ApplicationSection) {
+        showSection(section)
+    }
+}
+
+extension RootViewController: UINavigationControllerDelegate {
+
+    func navigationController(_ navigationController: UINavigationController,
+                              willShow viewController: UIViewController,
+                              animated: Bool) {
+        // Detect navigation to the root view when collapsed as this indicates there's no section selected.
+        // We also store the previously selected section to ensure we can return to this when expanding.
+        guard isCollapsed, viewController == libraryViewController,
+              let section = section
+        else {
+            return
+        }
+        previousSection = section
+        self.section = nil
+    }
+
+}
