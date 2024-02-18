inseven/opolua

commit 6a4bc89d7f196adc10c9b298b52b34b04086d16f

fix: Don't return "X" from GETCMD$ unless there's really been a quit event
Some apps incorrectly call GETCMD$ even when there hasn't been a 0x404
event meaning they were thinking they were supposed to quit when they
weren't.

src/opl.lua
@@ -873,6 +873,14 @@ function GETEVENTA32(stat, evAddr)
     local function completion()
         -- printf("GetEvent stat set to %s\n", stat())
         runtime:setResource("getevent", nil)
+        -- The GETCMD$ API is so stupid...
+        if stat() == KErrNone then
+            local ev = evAddr:asVariable(DataTypes.ELongArray)
+            if ev[KEvAType] == KEvCommand then
+                -- We never use KEvCommand for anything except quitCommand, so we can assume that here
+                runtime:setResource("GETCMD", "X")
+            end
+        end
     end
     local requestTable = {
         var = stat,

src/fns.lua
@@ -1219,8 +1219,9 @@ function UpperStr(stack, runtime) -- 0xD3
 end
 
 function WCmd(stack, runtime) -- 0xD5
-    -- This is the only cmd event we ever send, so...
-    stack:push("X")
+    local result = runtime:getResource("GETCMD")
+    runtime:setResource("GETCMD", nil)
+    stack:push(result or "")
 end
 
 function CmdStr(stack, runtime) -- 0xD6

commit 09f22062ffe2e83f4c03081c39c8685abb35bb01

fix: "Close Program" was not setting GETCMD=X correctly
I think this got broken somewhere in the memory model refactor, either
that or it's been broken ever since 6a4bc89...

src/opl.lua
@@ -871,7 +871,7 @@ function GETEVENTA32(stat, evAddr)
         -- The GETCMD$ API is so stupid...
         if stat() == KErrNone then
             local ev = evAddr:asVariable(DataTypes.ELongArray)
-            if ev[KEvAType] == KEvCommand then
+            if ev[KEvAType]() == KEvCommand then
                 -- We never use KEvCommand for anything except quitCommand, so we can assume that here
                 runtime:setResource("GETCMD", "X")
             end