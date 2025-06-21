e6c9e51191d41ce49c6fcb0c0d6943468271875d
Jason Morley <hello@jbmorley.co.uk>
2025-06-20 16:56:24 -1000

    Use the working directory

--- scripts/install-qt.sh
+++ scripts/install-qt.sh
@@ -1,12 +1,16 @@
 #!/bin/bash
 
+set -e
+set -o pipefail
+set -x
+set -u
 
-cd /tmp
+# cd /tmp
 curl -O https://qt.mirror.constant.com/archive/qt/6.9/6.9.1/single/qt-everywhere-src-6.9.1.tar.xz
 tar xf qt-everywhere-src-6.9.1.tar.xz
 mkdir -p qt-build
 # cd qt-build
-cd $(realpath qt-build)
+cd qt-build
 /tmp/qt-everywhere-src-6.9.1/configure
 ./configure
 cmake --build . --parallel
