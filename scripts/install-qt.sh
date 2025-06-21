819cb9c35b26633baf47dc1b52b1275adc050867
Jason Morley <hello@jbmorley.co.uk>
2025-06-20 16:51:34 -1000

    See if using the realpath works

--- scripts/install-qt.sh
+++ scripts/install-qt.sh
@@ -5,7 +5,8 @@ cd /tmp
 curl -O https://qt.mirror.constant.com/archive/qt/6.9/6.9.1/single/qt-everywhere-src-6.9.1.tar.xz
 tar xf qt-everywhere-src-6.9.1.tar.xz
 mkdir -p qt-build
-cd qt-build
+# cd qt-build
+cd $(realpath qt-build)
 /tmp/qt-everywhere-src-6.9.1/configure
 ./configure
 cmake --build . --parallel
