56473960825afacc25aacbc331f109eba7fa5077
Jason Morley <hello@jbmorley.co.uk>
2025-06-20 16:43:03 -1000

    Try compiling Qt

---
+++ scripts/install-qt.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+
+cd /tmp
+curl -O https://qt.mirror.constant.com/archive/qt/6.9/6.9.1/single/qt-everywhere-src-6.9.1.tar.xz
+tar xf qt-everywhere-src-6.9.1.tar.xz
+mkdir -p qt-build
+cd qt-build
+/tmp/qt-everywhere-src-6.9.1/configure
+./configure
+cmake --build . --parallel
