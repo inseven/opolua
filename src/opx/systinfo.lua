7786bd5133c6509d24f882f497f3d2e818699b60
Tom Sutcliffe <tomsci@me.com>
2025-11-02 11:28:27 +0000

    fix: Implement SITimeFormat in systinfo

--- src/opx/systinfo.lua
+++ src/opx/systinfo.lua
@@ -146,7 +146,23 @@ function SIDateFormat(stack, runtime) -- 4
 end
 
 function SITimeFormat(stack, runtime) -- 5
-    unimplemented("opx.systinfo.SITimeFormat")
+    local ampmPos = stack:pop():asVariable(DataTypes.ELong)
+    local ampmSpace = stack:pop():asVariable(DataTypes.ELong)
+    local timeSep3 = stack:pop():asVariable(DataTypes.EWord)
+    local timeSep2 = stack:pop():asVariable(DataTypes.EWord)
+    local timeSep1 = stack:pop():asVariable(DataTypes.EWord)
+    local timeSep0 = stack:pop():asVariable(DataTypes.EWord)
+    local timeFmt = stack:pop():asVariable(DataTypes.ELong)
+
+    timeFmt(runtime:LCClockFormat())
+    timeSep0(0)
+    timeSep1(string.byte(':'))
+    timeSep2(string.byte(':'))
+    timeSep3(0)
+    ampmSpace(0)
+    ampmPos(1)
+
+    stack:push(0)
 end
 
 function SIUTCOffset(stack, runtime) -- 6
