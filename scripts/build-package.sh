e913452cbd24a23b885ea1c5a30e86cb22e543e9
Jason Morley <hello@jbmorley.co.uk>
2025-12-29 14:27:24 -1000

    ci: Build the Swift package for iOS

---
+++ scripts/build-package.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+
+set -e
+set -o pipefail
+set -x
+
+ROOT_DIRECTORY="$( cd "$( dirname "$( dirname "${BASH_SOURCE[0]}" )" )" &> /dev/null && pwd )"
+
+cd "$ROOT_DIRECTORY"
+
+xcodebuild -scheme OpoLua -showdestinations
+
+# Build.
+xcodebuild -scheme OpoLua -destination "platform=macOS" clean build
+xcodebuild -scheme OpoLua -destination "$DEFAULT_IPHONE_DESTINATION" clean build
+
+# Test.
+xcodebuild -scheme OpoLua -destination "platform=macOS" test
+xcodebuild -scheme OpoLua -destination "$DEFAULT_IPHONE_DESTINATION" test
