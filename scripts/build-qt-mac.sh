6909d862f3963355fbcd4a2d635235a5b04fb030
Jason Morley <hello@jbmorley.co.uk>
2026-01-05 09:07:51 -1000

    Try again

--- scripts/build-qt-mac.sh
+++ scripts/build-qt-mac.sh
@@ -89,7 +89,7 @@ make
 mv "OpoLua.app" "OpoLua Qt.app"
 
 # Add the Qt libraries to the app bundle.
-macdeployqt -codesign "Developer ID Application: Jason Morley (QS82QFHKWB)"  "OpoLua Qt.app"
+macdeployqt -sign-for-notarization="Developer ID Application: Jason Morley (QS82QFHKWB)"  "OpoLua Qt.app"
 
 # Sign the app and prepare it for notarization.
 RELEASE_BASENAME="OpoLua-Qt-$VERSION_NUMBER-$BUILD_NUMBER"
