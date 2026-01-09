5b0f5ce2715f51af7c403cca0fdda7b5891f03af
Jason Morley <hello@jbmorley.co.uk>
2026-01-08 20:36:56 -1000

    See what user we’re running as

--- scripts/install-linux-dependencies.sh
+++ scripts/install-linux-dependencies.sh
@@ -1,5 +1,7 @@
 #!/bin/bash
 
+echo $USER
+
 sudo apt-get update -y
 sudo apt-get install -y build-essential git
 sudo apt-get install -y qt6-base-dev qt6-base-dev-tools qt6-multimedia-dev qt6-5compat-dev
