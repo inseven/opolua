9743f80bbae04d7334ba0fea8411c435275ea111
Jason Morley <hello@jbmorley.co.uk>
2026-01-08 20:34:13 -1000

    Use sudo

--- scripts/install-linux-dependencies.sh
+++ scripts/install-linux-dependencies.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
-apt-get update -y
-apt-get install -y build-essential git
-apt-get install -y qt6-base-dev qt6-base-dev-tools qt6-multimedia-dev qt6-5compat-dev
-apt-get install -y ruby ruby-bundler
-gem install --no-user-install fpm
+sudo apt-get update -y
+sudo apt-get install -y build-essential git
+sudo apt-get install -y qt6-base-dev qt6-base-dev-tools qt6-multimedia-dev qt6-5compat-dev
+sudo apt-get install -y ruby ruby-bundler
+sudo gem install --no-user-install fpm
